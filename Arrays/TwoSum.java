a6233de810ed25da3231b69519b3ba16118b122b
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2025-12-15 18:43:25 +0530

    DSA: Two Sum (Java) ✅

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -21,3 +21,25 @@ class Solution {
         return arr;
     }
 }
+
+/*
+Problem: <Two Sum>
+Topic: <HashMap>
+Approach: <Optimized - Use a HashMap to check if it contain other number , num = target- num[i>
+Time Complexity: O(𝑛)
+Space Complexity: O(n)
+*/
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int j = target-nums[i];
+            if(hm.containsKey(j)){
+                return new int[]{i, hm.get(j)};
+            }
+            hm.put(nums[i], i);
+        }
+        return new int[]{-1,-1};
+    }
+}
