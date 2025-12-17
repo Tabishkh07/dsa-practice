12efb8b1f2dee8e64a82341ab27812411a69bd2f
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2025-12-17 11:35:39 +0530

    DSA: Build Array From Permutation(Java)✅

--- Arrays/BuildArrayfromPermutation.java
+++ Arrays/BuildArrayfromPermutation.java
@@ -4,6 +4,7 @@ Topic: <Array>
 Approach: <brute force - simply follow the instructions>
 Time Complexity: O(n)
 Space Complexity: O(n)
+Run Time: 1ms
 */
 
 class Solution {
@@ -16,3 +17,26 @@ class Solution {
     }
 }
 
+/*
+Problem: <1920 Build Array from Permutation>
+Topic: <Array>
+Approach: <brute force - simply follow the instructions>
+Time Complexity: O(n)
+Space Complexity: O(n)
+Run time: 0ms
+*/
+class Solution {
+    public int[] buildArray(int[] nums) {
+        permutation(nums, 0);
+        return nums;
+    }
+
+    void permutation(int nums[], int start){
+        if(start<nums.length){
+        int temp=nums[start];
+        int ans=nums[temp];
+        permutation(nums, start+1);
+        nums[start]=ans;
+        }
+    }
+}
