3711334c2660c35ba6f2c0955cb18451e1bc182e
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2025-12-20 23:25:01 +0530

    DSA: Merge Sort Array(Java)✅

--- Arrays/MergeSortedArray.java
+++ Arrays/MergeSortedArray.java
@@ -16,3 +16,31 @@ class Solution {
     }
 }
 
+/*
+Problem: <88. Merge Sorted Array>
+Topic: <Array>
+Approach: <optimized - since last index are 0 of m and m>n we check from last index who ever is greater we put it in nums1 at m+n-1 
+position.>
+Time Complexity: O(n+m)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        while(m > 0 && n >0){
+            if(nums1[m-1] > nums2[n-1]){
+                nums1[m+n-1] = nums1[m-1];
+                m--;
+            }else{
+                nums1[m+n-1] = nums2[n-1];
+                n--;
+            }
+        }
+
+        // if few n are left
+        while(n>0){
+            nums1[m+n-1] = nums2[n-1];
+                n--;
+        }
+    }
+}
