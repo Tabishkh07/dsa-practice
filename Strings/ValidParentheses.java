443bb03615d274272eb2c07acbf3112229da871d
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2025-12-22 17:27:03 +0530

    DSA: Valid Parentheses(Java)✅

--- Strings/ValidParentheses.java
+++ Strings/ValidParentheses.java
@@ -15,7 +15,7 @@ class Solution {
         Stack<Character> st = new Stack<>();
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if(ch == '(' || ch=='{' || ch=='['){
+            if(ch == '(' || ch=='{'|| ch=='['){
                 st.push(ch);
             }
             else{
