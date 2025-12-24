057f20e56b76c7a09d3983fe7309e2f75f796393
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2025-12-24 23:50:58 +0530

    DSA: Remove Duplicate From Sorted Array(Java)✅

--- TwoPointers/RemoveDuplicateFromSortedArray.java
+++ TwoPointers/RemoveDuplicateFromSortedArray.java
@@ -1,6 +1,6 @@
 /*
 Problem: <26-Remove Duplicates from Sorted Array>
-Topic: <Array + Two Pointer>
+Topic: <Array + HashSet>
 Approach: <store in hashset to remove repeated elements then store them to array then patial sort till hashset length>
 Time Complexity: O(nlogn)
 Space Complexity: O(n)
@@ -21,3 +21,26 @@ class Solution {
     }
 }
 
+/*
+Problem: <26-Remove Duplicates from Sorted Array>
+Topic: <Array + Two Pointer>
+Approach: <take 2 pointers 1 for index other for traversing, if i != j then inc i and store nums at i>
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int i=0;
+        for(int j=1; j<nums.length; j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+        
+    }
+}
