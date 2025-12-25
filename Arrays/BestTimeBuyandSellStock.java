28832d3c92ba9beafccfd0db33782c0cce82a4bf
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2025-12-25 22:05:17 +0530

    DSA: Best Time Buy and Sell Stock(Java)✅

--- Arrays/BestTimeBuyandSellStock.java
+++ Arrays/BestTimeBuyandSellStock.java
@@ -22,7 +22,18 @@ class Solution {
 /*
 Problem: <121. Best Time to Buy and Sell Stock>
 Topic: <Array>
-Approach: <brute force just check all>
-Time Complexity: O(n^2)
+Approach: <which day to sell stock : find min all days and calculate profit daily>
+Time Complexity: O(n)
 Space Complexity: O(1)
 */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i=0; i<prices.length; i++){
+            min = Math.min(min, prices[i]);
+            profit = Math.max(profit, prices[i]-min);
+        }
+        return profit;
+    }
+}
