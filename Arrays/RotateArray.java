65912128aa8e1624f5d9097ef6b740e1705b6194
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2025-12-28 23:41:23 +0530

    DSA: Rotate Array(Java)✅

--- Arrays/RotateArray.java
+++ Arrays/RotateArray.java
@@ -3,7 +3,7 @@ Problem: <189 - Rotate Array>
 Topic: <Array>
 Approach: <can be done by using modulus operator - k%n index pe place it>
 Time Complexity: O(n)
-Space Complexity: O(n) not optimal.
+Space Complexity: O(n) not optimal can be done in O(1) space.
 */
 class Solution {
     public void rotate(int[] nums, int k) {
@@ -23,5 +23,28 @@ Problem: <189 - Rotate Array>
 Topic: <Array>
 Approach: <can be done by using modulus operator - k%n index pe place it>
 Time Complexity: O(n)
-Space Complexity: O(n) not optimal can be done in O(1) space.
+Space Complexity: O(1)
 */
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        // for cases where k>n
+        k = k%n; 
+        // 1st reverse the array - [7,6,5,4,3,2,1]
+        reverse(nums, 0, n-1);
+        // reverse the 1st half - [5,6,7,4,3,2,1]
+        reverse(nums, 0, k-1);
+        // reverse the 2nd half - [5,6,7,1,2,3,4]
+        reverse(nums, k, n-1);
+
+    }
+    public void reverse(int arr[], int l, int r){
+        while(l<r){
+            int temp = arr[r];
+            arr[r] = arr[l];
+            arr[l] = temp;
+            l++;
+            r--;
+        }
+    }
+}
