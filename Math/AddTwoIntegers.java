b3335fa9d08a397c8db7f26c301d6561fee2aff7
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-01-13 22:52:05 +0530

    DSA: Add Two Integers(Java)✅

---
+++ Math/AddTwoIntegers.java
@@ -0,0 +1,12 @@
+/*
+Problem: <2235: Add Two Integers>
+Topic: <Math Calculation>
+Approach: <simply add and return>
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
