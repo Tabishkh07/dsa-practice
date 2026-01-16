6529e5ddaea309f7f1cd5a9970c316c44f5ad732
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-01-16 22:21:17 +0530

    DSA: Sum Multiples(Java)✅

---
+++ Arrays/SumMultiples.java
@@ -0,0 +1,18 @@
+/*
+Problem: < 2652: Sum Multiples>
+Topic: <Array>
+Approach: <just check condition and add if yes>
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum=0;
+        for(int i=1; i<=n; i++){
+            if(i%3==0||i%5==0||i%7==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
