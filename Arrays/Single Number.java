5ac42a4853393d61bb9d6090af0cd36ac0ed77c9
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-06-03 09:22:01 +0530

    DSA: Single Number(JAVA)✅

--- Arrays/Single Number.java
+++ Arrays/Single Number.java
@@ -1,6 +1,31 @@
 /*
 Problem: <136. Single Number>
-Topic: <Array +HashMap>
+Topic: <Array>
+Approach: <Brute force>
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        for(int i=0; i<nums.length; i++){
+            int count =0;
+            for(int j=0; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                    count ++;
+                }
+            }
+            if(count < 2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
+
+/*
+Problem: <136. Single Number>
+Topic: <Array + HashMap>
 Approach: <Insert all values in HashMap along with frequency of apperance and then find the key with the help of value using entrySet>
 Time Complexity: O(n)
 Space Complexity: O(n)
@@ -21,4 +46,20 @@ class Solution {
     }
 }
 
+/*
+Problem: <136. Single Number>
+Topic: <Array>
+Approach: <Bit Manuplation - Using the special property of bit = duplicate no - 0 and single give same no>
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
 
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int num : nums){
+            ans ^=num;
+        }
+        return ans;
+    }
+}
