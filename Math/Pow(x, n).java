2f220f569d41811a0d4adb8bd4c4a4ed2d018dee
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-06-03 09:39:50 +0530

    DSA: Pow(x, n) (Java)✅

---
+++ Math/Pow(x, n).java
@@ -0,0 +1,21 @@
+/*
+Problem: <50: Pow(x, n)>
+Topic: <Math Calculation>
+Approach: <Brute Force - Using math function>
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public double myPow(double x, int n) {
+        return (double) Math.pow(x, n);
+    }
+}
+
+/*
+Problem: <50: Pow(x, n)>
+Topic: <Math Calculation>
+Approach: <Using math function>
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
