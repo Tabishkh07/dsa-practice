8cfa26dc5aca78aa57634d6e3548194c85897d00
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-06-04 10:05:41 +0530

    DSA: Sort Colors(Java)✅

--- TwoPointers/SortColors.java
+++ TwoPointers/SortColors.java
@@ -31,8 +31,35 @@ class Solution {
 /*
 Problem: < 75: Sort Colorr>
 Topic: <Array + Three Pointer>
-Approach: <Dutch National Flag Algorithm - 1 phase>
+Approach: <Dutch National Flag Algorithm - 1 phase => divide in 4 parts and arrange acc>
 Time Complexity: O(n)
 Space Complexity: O(1)
 */
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
 
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums, mid, low);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
