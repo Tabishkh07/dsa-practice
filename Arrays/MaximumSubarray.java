fa8afba9190226e8b1bedf579ea2ab6ed3c6c46a
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-06-04 06:33:50 +0530

    DSA: Maximum Subarray(Java)✅

--- Arrays/MaximumSubarray.java
+++ Arrays/MaximumSubarray.java
@@ -1,6 +1,30 @@
 /*
 Problem: <53-Maximum Subarray>
 Topic: <Array >
+Approach: <Apply brute force>
+Time Complexity: O(n^3)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum1=Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i; j<nums.length ; j++){
+                int sum=0;
+                for(int k=i; k<=j; k++){
+                    sum += nums[k];
+                }
+                sum1 = Math.max(sum1, sum);
+            }
+        }
+        return sum1;
+    }
+}
+
+/*
+Problem: <53-Maximum Subarray>
+Topic: <Array >
 Approach: <Apply Kadan Algo calculate curr and max sum>
 Time Complexity: O(n)
 Space Complexity: O(1)
