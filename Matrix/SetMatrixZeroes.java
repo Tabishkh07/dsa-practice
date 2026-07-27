d0e50ae2f2e06c5bf3a0941bd8d8df4cb9026d12
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-07-27 06:43:32 +0530

    DSA: Set Matrix Zeroes(JAVA)✅
    Implement algorithm to set matrix zeroes based on conditions.

---
+++ Matrix/SetMatrixZeroes.java
@@ -0,0 +1,31 @@
+/*
+Problem: <73. Set Matrix Zeroes>
+Topic: <Array >
+Approach: <Array for 0 on x and 0 on y axis , then store all zero pos in matrix and then later set matrix all 0 in row and col == 0>
+Time Complexity: O(n+m)
+Space Complexity: O(n+m)
+*/
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+      boolean x[] = new boolean[matrix.length];
+      boolean y[] = new boolean[matrix[0].length];
+
+      for(int i=0; i<matrix.length; i++){
+        for(int j=0; j<matrix[0].length; j++){
+          if(matrix[i][j] == 0){
+            x[i] = true;
+            y[j] = true;
+          }
+        }
+      }
+
+      for(int i=0; i<matrix.length; i++){
+        for(int j=0; j<matrix[0].length; j++){
+          if(x[i] || y[j]){
+            matrix[i][j] = 0;
+          }
+        }
+      }
+    }
+}
