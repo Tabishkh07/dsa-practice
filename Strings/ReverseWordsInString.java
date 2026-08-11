45ba69b91651bb9093052007d3a586df399eab0d
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-08-11 07:39:32 +0530

    DSA: Reversse Words In String(Java)✅

--- Strings/ReverseWordsInString.java
+++ Strings/ReverseWordsInString.java
@@ -31,3 +31,27 @@ class Solution {
         return sb.toString();
     }
 }
+
+/*
+Problem: <151. Reverse Words in a String>
+Topic: <String + >
+Approach: <Using Split>
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+}
