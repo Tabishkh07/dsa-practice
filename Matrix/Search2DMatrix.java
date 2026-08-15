7d4c7188de7d1cbbda457111fa9d1ef97425ad93
Mohammed Tabish Khan <161915192+Tabishkh07@users.noreply.github.com>
2026-08-15 07:44:33 +0530

    DSA: Search2DMatrix(Java)✅
    Implemented binary search for a 2D matrix to find a target value.

--- Matrix/Search2DMatrix.java
+++ Matrix/Search2DMatrix.java
@@ -23,3 +23,36 @@ class Solution {
         return false;
     }
 }
+
+/*
+Problem: <74. Search a 2D Matrix>
+Topic: <Array + matrix>
+Approach: <**Approach:** Treat the 2D matrix as a sorted 1D array and apply binary search, converting `mid` into `(row, col)` using `/` and `%`.
+>
+Time Complexity: O(log m*n)
+Space Complexity: O(1)
+*/
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int end=cols * rows - 1;
+        int start=0;
+        while(start <= end ){
+
+            int mid = (start+end)/2;
+            int row = mid/cols;
+            int col = mid%cols;
+
+            if(matrix[row][col] < target){
+                start = mid+1;
+            }else if(matrix[row][col] > target){
+                end = mid-1;
+            }else{
+                return true;
+            }   
+        }    
+        return false;
+    }
+}
